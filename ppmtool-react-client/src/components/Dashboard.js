87d50898426707782fde393d0abafae50b784aa3
Fran <Fran@DESKTOP-NO101JL>
2020-06-05 19:01:46 +0200

    first component

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+    render() {
+        return <h1>Welcome to the Djiashboard</h1>    
+    }
+}
+
+//export default Dashboard;
