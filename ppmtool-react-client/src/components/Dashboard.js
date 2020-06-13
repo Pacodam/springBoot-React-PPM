1c844e030e5e251f41c635c0d677b1c7622dfc4a
Fran <Fran@DESKTOP-NO101JL>
2020-06-13 18:19:44 +0200

    get projects final step

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -14,6 +14,9 @@ class Dashboard extends Component {
   }
 
   render() {
+    
+    const {projects} = this.props.project
+
     return (
       <div className="projects">
         <div className="container">
@@ -24,7 +27,13 @@ class Dashboard extends Component {
                     <CreateProjectButton />
                     <br />
                     <hr />
-                    <ProjectItem />
+                    {
+                      projects.map(project => (
+                      <ProjectItem key = {project.id} project={project} />
+                    ))
+                       
+                    }
+                   
                 </div>
             </div>
         </div>
